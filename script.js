aa86c194c3008a7a1651a3b0e4530aa7cfcf9658
RenativenkatasandeepRVS <96691109+RenativenkatasandeepRVS@users.noreply.github.com>
2025-05-25 17:22:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const track = document.querySelector('.carousel-track');
+const slides = document.querySelectorAll('.project-slide');
+const nextBtn = document.querySelector('.carousel-btn.next');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+let currentIndex = 0;
+
+function updateCarousel() {
+  const slideWidth = slides[0].offsetWidth;
+  track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);
